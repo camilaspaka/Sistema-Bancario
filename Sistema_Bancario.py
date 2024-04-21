c6689dc0ec6f3955b61acf160c0f8fc8159e8bb2
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-21 16:11:48 -0300

    Update Sistema_Bancario.py

--- Sistema_Bancario.py
+++ Sistema_Bancario.py
@@ -27,17 +27,46 @@ Limite_Saques = 3
 while True:
 
     opcao = input(menu)
+
     if opcao == "E":
-        print("Extrato")
+        print("\n=========================Extrato==============================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo R$ {saldo:.2f}")
+        print("________________________________________________________________")
+
     elif opcao == "D":
-        print("Deposito")
+        valor = float(input("Informe o valor do deposito: "))
+        if valor > 0:
+            saldo += valor
+            extrato += f"Deposito : R$ {valor:.2f}\n"
+        else:
+            print("Operação falhou!")
+
     elif opcao == "I":
-        print("Emprestimo")
+        valor = float(input("Qual o valor do emprestimo que deseja realizar?: "))
+        if valor > 5000:
+            print("Operação falhou! Valor acima do permitido para emprestimo nessa conta!")
+        else:    
+            print("Emprestimo de R$ {valor:.2f} realizado com sucesso!!!")
+
     elif opcao == "S":
-        print("Saque")
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= Limite_Saques
+
+        if excedeu_saldo:
+            print("Operação não realizada. Realize um emprestimo ou deposite mais dinheiro na conta.") 
+        if excedeu_limite:
+            print("Operação falhou! Você excedeu o limite diario.")
+        if excedeu_saques: 
+            print("Operação falhou! Você não pode mais realizar nenhum saque hoje")
+
+
     elif opcao == "Q":
         break
 
-
+ 
     else:
         print("Operação invalida, por favor selecione uma opção valida")
